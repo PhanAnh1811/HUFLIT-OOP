72ede6eaa5056f3835ab2cd000c659a2e59999c1
PhanAnh1811 <tuananh.61779@gmail.com>
2020-12-14 22:18:04 +0700

    update

--- Buoi_9/OnTap/Staff.cs
+++ Buoi_9/OnTap/Staff.cs
@@ -8,6 +8,17 @@ namespace OnTap
     {
         private string staffCode;
         private string salary;
+        private string countribute;
 
+        public Staff(string staffCode, string salary, string countribute)
+        {
+            this.staffCode = staffCode;
+            this.salary = salary;
+            this.countribute = countribute;
+        }
+
+        public string StaffCode { get => staffCode; set => staffCode = value; }
+        public string Salary { get => salary; set => salary = value; }
+        public string Countribute { get => countribute; set => countribute = value; }
     }
 }
